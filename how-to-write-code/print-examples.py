9f91b5e4a57c60e6ace5c7dbd7e1e26b1dbac9df
Zeal Gonzalez <zealgonzalez@Zeals-MBP.lan>
2024-08-21 19:21:52 -0500

    newline-examples

--- how-to-write-code/print-examples.py
+++ how-to-write-code/print-examples.py
@@ -1,8 +1,8 @@
-print('Hello, JavaScript.');
+print('Hello\nJavaScript.');
 print(2001);
-print("What","do","commas","do?");
-print("Does", "adding",      "space", "matter?");
-print('Launch' + 'Code');
+print("What\tdo\tcommas\tdo?");
+print("Does\tadding\nspace\tmatter?");
+print('Launch\tCode');
 print("LaunchCode was founded in", 2013);
 
 #In line 4 when they add commas it seperates the words from becoming one long phrase
