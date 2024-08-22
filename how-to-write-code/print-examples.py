f6980a5ff1705bb4c61d64200a9f25ec95b8f77d
Zeal Gonzalez <zealgonzalez@Zeals-MBP.lan>
2024-08-21 19:16:16 -0500

    print-examples

--- how-to-write-code/print-examples.py
+++ how-to-write-code/print-examples.py
@@ -4,3 +4,5 @@ print("What","do","commas","do?");
 print("Does", "adding",      "space", "matter?");
 print('Launch' + 'Code');
 print("LaunchCode was founded in", 2013);
+
+#In line 4 when they add commas it seperates the words from becoming one long phrase
