6494e0c926c1c0ac942ef8d52ee726922fa43f36
Zeal Gonzalez <zealgonzalez@Zeals-MacBook-Pro.local>
2024-08-23 11:43:59 -0500

    booleans-and-conditionals

--- booleans-and-conditionals/exercises01.py
+++ booleans-and-conditionals/exercises01.py
@@ -1,9 +1,18 @@
 # Assign the variables for exercise 1 here:
-
-
-
+engine_indicator_light = "red blinking"
+print(engine_indicator_light);
+space_suits_on = True
+print(space_suits_on);
+shuttle_cabin_ready = True
+print(space_suits_on);
+crew_status = space_suits_on and shuttle_cabin_ready
+print(crew_status);
+computer_status_code = 200
+print(computer_status_code);
+shuttle_speed = 15000
+print(shuttle_speed)
 # BEFORE running the code, predict what will be printed to the console by the following statements:
-
+# I predict the outcome will give us "engines are off"
 if engine_indicator_light == "green": 
   print("engines have started")
 elif engine_indicator_light == "green blinking": 
--- booleans-and-conditionals/exercises02.py
+++ booleans-and-conditionals/exercises02.py
@@ -1,17 +1,26 @@
 engine_indicator_light = "red blinking"
 space_suits_on = True
 shuttle_cabin_ready = True
-crewStatus = space_suits_on and shuttle_cabin_ready
-computerStatusCode = 200
+crew_status = space_suits_on and shuttle_cabin_ready
+computer_status_code = 200
 shuttleSpeed = 15000
 
 # 3) Write conditional expressions to satisfy the following safety rules:
-
+if shuttle_cabin_ready:
+   print("Cabin Ready")
+else:
+   print("Cabin Not Ready")
 # a) If crew_status is true, print "Crew Ready" else print "Crew Not Ready".
-
+if crew_status:
+   print("Ready for take off")
+else:
+   print("Not ready for take off")
 
 # b) If computer_status_code is 200, print "Please stand by. Computer is rebooting." Else if computer_status_code is 400, print "Success! Computer online." Else print "ALERT: Computer offline!"
-
+if computer_status_code:
+   print("Computer on")
+else:
+   print("Compuer off")
 
 # c) If shuttle_speed is > 17,500, print "ALERT: Escape velocity reached!" Else if shuttle_speed is < 8000, print "ALERT: Cannot maintain orbit!" Else print "Stable speed".
 
