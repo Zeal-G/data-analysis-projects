05cd3cb81c9cdeda73c6820df48446a1bf781f62
Zeal Gonzalez <zealgonzalez@Zeals-MacBook-Pro.local>
2024-08-27 20:03:43 -0500

    test

--- collections/exercises/lists.py
+++ collections/exercises/lists.py
@@ -1,18 +1,21 @@
 # Create the adding_practice list with the following entry: 273.15
 
+my_list = [273.15]
+
 # Use the append method to add the number 42 and the string "hello" to the list. Add these new items one at a time.  Print the list after each step to confirm the changes.
-
+my_list.append(42)
+my_list.append("hello")
 # Use list concatenation to add these three items to the list all at once: [False, -4.6, '87'].
-
+my_list += [False, -4.6, '87']
 # Use the cargo_hold list for the next set of exercises.
 cargo_hold = ['oxygen tanks', 'space suits', 'parrot', 'instruction manual', 'meal packs', 'slinky', 'security blanket']
 
 # Use bracket notation to replace 'slinky' in the list with 'space tether'. Print the list to confirm the change.
-
+cargo_hold[5] = 'slinky'
 
 # Remove the last item from the list with pop. Print the element removed and the updated list.
 
-
+ 
 # Remove the first item from the list with pop. Print the element removed and the updated list.
 
 
