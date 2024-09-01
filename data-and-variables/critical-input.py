b3640b7e6710efea7ce6f64204a09750f935d0d1
Zeal <lovingzeal@outlook.com>
2024-09-01 15:54:07 -0500

    data-analysis-projects

--- data-and-variables/critical-input.py
+++ data-and-variables/critical-input.py
@@ -2,3 +2,22 @@ num1 = 8
 num2 = 10
 
 print(num1 + num2)
+
+#num1 = input("Enter a number: ")
+#num2 = input("Enter another number: ")
+
+#print(num1 + num2)
+
+info = input(25)
+# The user enters 25.
+
+print(type(info))
+
+#1. string - correct answer
+#1. number
+#1. info
+#1. 25
+
+Birthday = input('February 8')
+#print(Birthday)
+print(type(Birthday))
