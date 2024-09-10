38075e2d1fdb13f336c0699c8c8a09aec05d6a50
Zeal <lovingzeal@outlook.com>
2024-09-10 18:29:17 -0500

    collections

--- loops/exercises/Loop-examples.py
+++ loops/exercises/Loop-examples.py
@@ -99,3 +99,4 @@ def display_invoices(username, amount, due_date):
    print(f"Hello {username}")
    print(f"Your bill of ${amount: .2f} is due {due_date}")
 display_invoices("Bro", 42.50, "01/01")
+#example from youtube
